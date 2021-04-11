615877b4a86333ac2d6825da48bf64464d46d657
atvf000 <47750049+atvf000@users.noreply.github.com>
2021-04-11 17:05:56 +0300

    [WIP] LR2

--- ITIB_LR2/main.py
+++ ITIB_LR2/main.py
@@ -12,10 +12,10 @@ class NeuralNetwork:
         self.__p = p
         self.__M = M
         self.__k = 0
-        self.__net = 0
         self.__weight = [0] * (p + 1)
         self.__x = []
         self.__y = []
+        self.__error = 0
 
     def generateX(self):
         x = []
@@ -31,20 +31,23 @@ class NeuralNetwork:
     def func(self, x):
         return 0.5 * math.cos(0.5 * x) - 0.5
 
-    def countNet(self, y):
-        self.__net = 0
-        for i in range(self.__p + 1):
-            self.__net += y[i] * self.__weight[i]
+    def countNet(self, i):
+        net = 0
+        for j in range(1, self.__p + 1):
+            net += self.__y[i + j] * self.__weight[j]
+        return net
 
-    def funcActivation(self):
-        return self.__net
+    def weightCorrect(self, sigma, i):
+        for j in range(1, self.__p + 1):
+            self.__weight[j] = self.__weight[j] + self.countDeltaWeight(sigma, self.__y[i + j])
 
-    def weightCorrect(self, sigma, y):
-        for i in range(self.__p + 1):
-            self.__weight[i] = self.__weight[i] + self.countDeltaweight(sigma, y[i])
+    def countDeltaWeight(self, sigma, y_j):
+        return sigma * self.__eta * y_j
 
-    def countDeltaweight(self, sigma, x_i):
-        return sigma * self.__eta * x_i
+    def countError(self, i):
+        for j in (1,self.__p):
+            self.__error += (self.__x[i] - self.__x[i + j]) ** 2
+        self.__error = math.sqrt(self.__error)
 
     def study(self):
         self.generateX()
@@ -53,21 +56,17 @@ class NeuralNetwork:
         while self.__k < self.__M:
 
             for i in range(self.__p, self.__N):
-                self.__y[i] = self.countNet()
+                self.__y[i] = self.countNet(i)
+
+                self.__x[i] = self.__x[-1] + (abs(self.__b) + abs(self.__a)) / self.__N
+                self.weightCorrect(self.func(self.__x[i]) - self.__y[i], i)
+                self.countError(i)
 
             self.__k += 1
 
-
-#def printGraph(errors):
-
-
-
-
 def start():
 
-
-
-    nw = NeuralNetwork(0.3)
+    nw = NeuralNetwork(1, 20, -5, 5, 4, 100)
     nw.study()
 
 
